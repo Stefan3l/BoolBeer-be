a903672ce55aef7587a3aaf2bfbc6896e15b6eef
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-05-06 17:36:05 +0200

    feat: zoom sul singolo prodotto

--- resources/views/admin/beers/show.blade.php
+++ resources/views/admin/beers/show.blade.php
@@ -9,10 +9,13 @@
             </div>
             <div class="bg-white dark:bg-gray-800 shadow-2xl rounded-lg overflow-hidden hover:shadow-3xl transition-shadow duration-300">
                 <div class="flex flex-col md:flex-row">
-                    <div class="w-full md:w-1/3 bg-gray-100 dark:bg-gray-700">
-                        <img src="{{asset('../images/' . $beer->image)}}" 
-                             alt="{{$beer->name}}" 
-                             class="w-full h-[400px] object-cover hover:scale-105 transition-transform duration-300">
+                    <div class="w-full md:w-1/3 bg-gray-100 dark:bg-gray-700 relative">
+                        <div class="img-magnifier-container">
+                            <img id="beerImage" 
+                                 src="{{asset('../images/' . $beer->image)}}" 
+                                 alt="{{$beer->name}}" 
+                                 class="w-full h-[400px] object-cover">
+                        </div>
                     </div>
                     <div class="w-full md:w-2/3 p-8">
                         <div class="mb-6">
@@ -70,4 +73,78 @@
             </div>
         </div>
     </div>
+
+    <style>
+        .img-magnifier-container {
+            position: relative;
+        }
+        .img-magnifier-glass {
+            position: absolute;
+            border: 3px solid #000;
+            border-radius: 50%;
+            cursor: none;
+            width: 100px;
+            height: 100px;
+            display: none;
+        }
+    </style>
+
+    <script>
+        function magnify(imgID, zoom) {
+            var img, glass, w, h, bw;
+            img = document.getElementById(imgID);
+            
+            glass = document.createElement("DIV");
+            glass.setAttribute("class", "img-magnifier-glass");
+            img.parentElement.insertBefore(glass, img);
+
+            glass.style.backgroundImage = "url('" + img.src + "')";
+            glass.style.backgroundRepeat = "no-repeat";
+            glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+            bw = 3;
+            w = glass.offsetWidth / 2;
+            h = glass.offsetHeight / 2;
+
+            img.addEventListener("mousemove", moveMagnifier);
+            glass.addEventListener("mousemove", moveMagnifier);
+            
+            img.addEventListener("mouseenter", function() {
+                glass.style.display = "block";
+            });
+            
+            img.addEventListener("mouseleave", function() {
+                glass.style.display = "none";
+            });
+
+            function moveMagnifier(e) {
+                var pos, x, y;
+                e.preventDefault();
+                pos = getCursorPos(e);
+                x = pos.x;
+                y = pos.y;
+
+                if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+                if (x < w / zoom) {x = w / zoom;}
+                if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+                if (y < h / zoom) {y = h / zoom;}
+
+                glass.style.left = (x - w) + "px";
+                glass.style.top = (y - h) + "px";
+                glass.style.backgroundPosition = "-" + ((x * zoom) - w) + "px -" + ((y * zoom) - h) + "px";
+            }
+
+            function getCursorPos(e) {
+                var a, x = 0, y = 0;
+                e = e || window.event;
+                a = img.getBoundingClientRect();
+                x = e.pageX - a.left - window.pageXOffset;
+                y = e.pageY - a.top - window.pageYOffset;
+                return {x : x, y : y};
+            }
+        }
+
+        document.addEventListener("DOMContentLoaded", function() {
+            magnify("beerImage", 3);
+        });
+    </script>
 </x-app-layout>
