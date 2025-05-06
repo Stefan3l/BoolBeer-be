b98e397ec67db0475ea798f8ee67248d9092e28b
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-05-06 10:28:19 +0200

    fix: message conferma Log-in

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg ">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div x-data="{ open: true }" x-show="open" class="p-6 text-gray-900 dark:text-gray-100 relative">
                     {{ __("You're logged in!") }}
+                    <button @click="open = false" class="absolute top-2 right-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+                        <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
