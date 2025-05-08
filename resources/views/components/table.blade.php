66c718785dc1c890e54117b0cbb8195a4de8df7f
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-05-08 21:09:51 +0200

    fix: img table

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -4,7 +4,7 @@
     <table class="w-full text-sm text-left text-gray-800 dark:text-gray-200">
         <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3">Image</th>
+                <th scope="col" class="px-6 py-3 text-center">Image</th>
                 <th scope="col" class="px-6 py-3">Name</th>
                 <th scope="col" class="px-6 py-3">Quantity</th>
                 <th scope="col" class="px-6 py-3">Alcohol Content</th>
@@ -14,8 +14,8 @@
         <tbody>
             @foreach($beers as $beer)
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td class="px-6 py-4">
-                        <img src="{{asset('storage/public/images/' . $beer->image)}}" alt="{{$beer->name}}" class="w-16 h-16 object-cover rounded-full">
+                    <td class="px-6 py-4 flex justify-center items-center">
+                        <img src="{{asset('storage/public/images/' . $beer->image)}}" alt="{{$beer->name}}" class="w-24 h-24 object-cover rounded-full">
                     </td>
                     <td class="px-6 py-4 font-bold text-xl text-gray-900 dark:text-white whitespace-nowrap">{{$beer->name}}</td>
                     <td class="px-6 py-4">{{$beer->quantity}}cl</td>
